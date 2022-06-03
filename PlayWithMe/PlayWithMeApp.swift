f96c7c9e5a8d5512042c2c5b39ca56d17b7ff0df
Anton Muravev <antonmuravev@mail.com>
2022-06-03 15:01:45 +0100

    Initial Commit

---
+++ PlayWithMe/PlayWithMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayWithMeApp.swift
+//  PlayWithMe
+//
+//  Created by crode on 03/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct PlayWithMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
